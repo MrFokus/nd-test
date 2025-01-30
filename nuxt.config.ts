d7f648424f1d5a814bd31064ea9f9086db8525ea
Глубоков Герман <glubokovgerman@MacBook-Pro-3.local>
2025-01-30 15:08:59 +0300

    add assets materials

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr:false
+  ssr:false,
+  css: ['~/assets/style/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/style/variables.scss" as *;'
+        }
+      }
+    }
+  }
 })
