e48bd75836efbbd1ff55420fa431a6f6e1ace153
Глубоков Герман <glubokovgerman@MacBook-Pro-3.local>
2025-01-30 17:05:27 +0300

    add index page

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="page-container">
+    <LogoIcon class="logo" />
+    <button class="primary sign-in"><SignInIcon/>Вход</button>
+  </header>
+  <main class="flex-col">
+    <slot />
+  </main>
+</template>
+
+<script setup lang="ts">
+import LogoIcon from "@/assets/img/logo.svg?component";
+import SignInIcon from "@/assets/img/sign-in.svg?component";
+</script>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  justify-content: space-between;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  right: 0;
+  left: 0;
+  background-color: $dark;
+}
+.sign-in{
+    &>svg{
+        width: 34px;
+    }
+}
+
+.logo{
+    height: 50px;
+}
+main{
+    flex: 1 0 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,38 @@
 <template>
+  <section class="welcome page-container flex">
+    <div class="title-container flex-col">
+        <h1 class="h1">Мои <br> заметки</h1>
+        <p class="h3 color-gray">Не забывай о важном,<br> храни его в облаке.</p>
+    </div>
+    <div class="preview"/>
+  </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
 
-<style scoped></style>
+</script>
+
+<style scoped lang="scss">
+.welcome{
+    flex: 1 0 0;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 100%;
+    max-height: 100%;
+    gap: 90px;
+}
+.title-container {
+    gap: 40px;
+    width: fit-content;
+}
+.preview{
+  display: flex;
+  flex: 1 0 0;
+  height: 100%;
+  background-image: 100%;
+  background-position: right;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-image: url("@/assets/img/index-preview.svg");
+}
+</style>
